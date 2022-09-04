f460d40a846f7f65364e25fbc624d69f5bad9efa
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2022-09-04 12:34:24 +0300

    lispify problems read lispified problems

---
+++ arturs/lispify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sed "s/^/(process '/" \
+| sed "s/$/)/" \
+| sed "s/\[x\]/'x/g" \
+| sed "s/\[y\]/'y/g" \
+| sed "s/\[X\]/'x/g" \
+| sed "s/\[Y\]/'y/g" \
+| sed "s/\]/)/g" \
+| sed "s/\[/(list /g" \
+| sed "s/\./ /g" \
+| sed "s/\,/ /g"
+
+
