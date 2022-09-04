44da77deaa0faef588941182a9bbc508f9e39f1d
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2022-09-04 09:24:42 +0300

    more pretty json loading

--- arturs/run.sh
+++ arturs/run.sh
@@ -6,6 +6,5 @@ if [[ $1 -le 0 || $1 -gt 25 ]]; then
 fi
 S=$(head -n $1 mosaic-run/scores.txt | tail -n 1 | sed "s/.*(//" | sed "s/)//")
 sbcl --noinform \
-     --eval '(ql:quickload "png-read")' \
      --eval '(load "main.lisp")' \
      --eval "(top-level $1 $S $S)"
